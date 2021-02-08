1b38658273f27bed181fbf947cd9d84492d7efd8
Andrew Miller <jazznerd206@icloud.com>
2021-02-08 15:23:52 -0800

    add sorts

--- algos/web_dev/stock_price.py
+++ algos/web_dev/stock_price.py
@@ -7,23 +7,13 @@ def stock_price(symbol):
     url = "https://in.finance.yahoo.com/quote/" + symbol + "?s=" + symbol + '"'
     print('finding...')
     soup = BeautifulSoup(requests.get(url).text, "html.parser")
-    # print(soup)
     class_ = "My(6px) Pos(r) smartphone_Mt(6px)"
     return soup.find("div", class_=class_).find("span").text
 
 def job(ticker):
-    return stock_price(ticker)
-    time.sleep(1)
+    print(stock_price(ticker))
 
-# def ez_job():
-#     print('test job')
-
-# print('ARKG', stock_price("ARKG"))
-# print('SPCE', stock_price("SPCE"))
-# print('NOK', stock_price("NOK"))
-
-if __name__ == '__main__':
-    ticker = input('ticker: ')
-    while True:
-        job(ticker)
-    
+ticker = input('ticker: ')
+job(ticker)
+time.sleep(1)
+hourly = []
