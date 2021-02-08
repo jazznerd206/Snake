6475a2d23bd6be504c79b4698bee5c845d2bf8ab
Andrew Miller <jazznerd206@icloud.com>
2021-02-08 08:31:37 -0800

    add file structure

---
+++ diff_between_iterables.py
@@ -0,0 +1,9 @@
+# difference
+# Returns the difference between two iterables.
+
+# Use list comprehension to only keep values not contained in b
+
+def difference(a, b):
+    return [item for item in a if item not in b]
+
+print(difference([1, 2, 3], [1, 2, 4])) #3
