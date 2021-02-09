87e6c6f1e58c257725b216b448163f2088cac2f2
Andrew Miller <jazznerd206@icloud.com>
2021-02-09 11:59:17 -0800

    job scraper CLI

--- algos/web_dev/get_jobs.py
+++ algos/web_dev/get_jobs.py
@@ -1,23 +1,29 @@
-
-from __future__ import annotations
-
-from typing import Generator
-
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-url = "https://www.indeed.co.in/jobs?q=front+end+development&l="
-
-
-def fetch_jobs(location: str = "mumbai") -> Generator[tuple[str, str], None, None]:
-    soup = BeautifulSoup(requests.get(url + location).content, "html.parser")
-    # This attribute finds out all the specifics listed in a job
+def fetch_jobs(job_type, location):
+    job_in_url = job_type.lower().split(' ')
+    job_list = "+".join(job_in_url)
+    url = "https://www.indeed.com/jobs?q="
+    joiner = "&l="
+    soup = BeautifulSoup(requests.get(url + job_list + joiner + location).content, "html.parser")
     for job in soup.find_all("div", attrs={"data-tn-component": "organicJob"}):
-        job_title = job.find("a", attrs={"data-tn-element": "jobTitle"}).text.strip()
+        job_anchor = job.find("a", attrs={"class": "turnstileLink"})
+        job_title = job_anchor.text.strip()
+        job_link = urljoin(url,job_anchor.get("href"))
+        easy_apply = False
+        easy_apply_tag = job.find("span", attrs={"class": "iaLabel iaIconActive"})
         company_name = job.find("span", {"class": "company"}).text.strip()
-        yield job_title, company_name
+        if easy_apply_tag:
+            easy_apply = True
+        yield job_title, company_name, job_link, easy_apply
 
-
-if __name__ == "__main__":
-    for i, job in enumerate(fetch_jobs("Bangalore"), 1):
-        print(f"Job {i:>2} is {job[0]} at {job[1]}")
+job_type = input('Enter job title: ')
+zip_code = input("Zip Code: ")
+for i, job in enumerate(fetch_jobs(job_type, zip_code), 1):
+    print(f"================== Job{i:>4} ===================")
+    print("----------------------------------------------")
+    print(f"{job[0]} at {job[1]}.\nLink :{job[2]} \nEasy apply? {job[3]}")
+    print("----------------------------------------------")
+    print("==============================================")
